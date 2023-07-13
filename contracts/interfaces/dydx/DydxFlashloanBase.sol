05201d80383d542a1b9ecceca111a8b37b3c8488
welcomefrank <25167698+welcomefrank@users.noreply.github.com>
2023-07-13 16:27:06 +0800

    Update DydxFlashloanBase.sol

--- contracts/interfaces/dydx/DydxFlashloanBase.sol
+++ contracts/interfaces/dydx/DydxFlashloanBase.sol
@@ -5,7 +5,8 @@ pragma experimental ABIEncoderV2;
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import "./ISoloMargin.sol";
+//import "./ISoloMargin.sol";
+import "https://raw.githubusercontent.com/welcomefrank/defi-by-example/main/contracts/interfaces/dydx/ISoloMargin.sol";
 
 contract DydxFlashloanBase {
   using SafeMath for uint;
